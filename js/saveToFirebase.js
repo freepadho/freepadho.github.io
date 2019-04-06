46f1295e7cbc73933e655510817c6c89622d30a2
Vishal Kushwaha <vishsinkush@gmail.com>
2019-04-06 20:40:40 +0530

    alert fixed

--- js/saveToFirebase.js
+++ js/saveToFirebase.js
@@ -10,7 +10,7 @@ function saveToFirebase() {
         .then(function(snapshot) {
             //success(); // some success method
             document.getElementById('success').className = "alert alert-success";
-            document.getElementById('success').innerHTML = "Successful!";
+            document.getElementById('success').innerHTML = "Submitted successfuly!";
         }, function(error) {
             document.getElementById('success').className = "alert alert-danger";
             document.getElementById('success').innerHTML = "Something went wrong!";
@@ -22,7 +22,7 @@ function saveToFirebase() {
 }
 //$("#saveToFirebase").bind("click",saveToFirebase);
 function init(){
-    document.getElementById('new_form').onsubmit = saveToFirebase;
+    document.getElementById('new-form').onsubmit = saveToFirebase;
 }
 window.onload = init;
 
