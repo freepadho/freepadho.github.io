194762cf0ecb90a8cb18fa0af6d9b79c73e76743
Vishal Kushwaha <vishsinkush@gmail.com>
2019-04-06 20:20:59 +0530

    added alert

--- js/saveToFirebase.js
+++ js/saveToFirebase.js
@@ -9,11 +9,11 @@ function saveToFirebase() {
     firebase.database().ref('subscription-entries').push().set(emailObject)
         .then(function(snapshot) {
             //success(); // some success method
-            document.getElementById(success).className = "alert alert-success";
-            document.getElementById(success).innerText = "Successful!";
+            document.getElementById('success').className = "alert alert-success";
+            document.getElementById('success').innerText = "Successful!";
         }, function(error) {
-            document.getElementById(success).className = "alert alert-danger";
-            document.getElementById(success).innerText = "Something went wrong!";
+            document.getElementById('success').className = "alert alert-danger";
+            document.getElementById('success').innerText = "Something went wrong!";
             console.log('error' + error);
             //error(); // some error method
         });
